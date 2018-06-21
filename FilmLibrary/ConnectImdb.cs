cefdb498c4a845c8da8cb47d2b5af3c007adf1d4
Adelina <->
2018-06-21 21:31:00 +0300

    Add example retrieve rating value from Imdb for a certain movie

---
+++ FilmLibrary/ConnectImdb.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmLibrary
+{
+  class ConnectImdb
+  {
+    public static string retrieveRatingValue()
+    {
+      string ratingValue = "";
+      //Shrek link 
+      Uri uri = new Uri("http://www.imdb.com/title/tt0892791/");
+      WebClient site = new WebClient();
+      //Html from page
+      string html = site.DownloadString(uri);
+      //Title html tags
+      string ratingValueTag = "<span itemprop=\"ratingValue\">";
+      int indexA = html.IndexOf(ratingValueTag);
+
+      //Found the tages
+      if (indexA != -1)
+      {
+
+        int indexB = html.IndexOf("</span>", indexA);
+
+        //Found second tages
+        if (indexB != -1)
+        {
+          //index of will get the start of the search, we want the end
+          indexA = indexA + ratingValueTag.Length;
+          //Title is inbetween range
+          ratingValue = html.Substring(indexA, indexB - indexA);
+        }
+      }
+
+      return ratingValue;
+    }
+  }
+}
--- FilmLibrary/Form1.cs
+++ FilmLibrary/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,48 +13,33 @@ namespace FilmLibrary
 {
   public partial class Form1 : Form
   {
-
-        
-    
     public Form1()
     {
       InitializeComponent();
     }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
+    private void textBox1_TextChanged(object sender, EventArgs e)
+    {
 
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            string value = textBox1.Text;
-
-
-            string host = "imdb.com";
-            int port = 80;
-
-            string result = ConnectImdb.SocketSendReceive(host, port);
-            Console.WriteLine(result);
-
-
-
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
     }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      string value = ConnectImdb.retrieveRatingValue();
+      //TODO: implement this
+      //demo test
+      textBox1.Text = value;
+    }
+
+
+    private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+    {
+
+    }
+  }
 }
